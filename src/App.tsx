2cf086d93301820c2d991dcf28b4bb36fe219f08
cookieMonsterDev <mykhailo.toporkov@gmail.com>
2023-03-04 18:56:13 +0200

    formik + use select

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,39 @@
+import Select from 'react-select';
+import { useFormik } from 'formik';
+
+interface Options {
+  value: string;
+  label: string;
+}
+
+const options = [
+  { value: 'chocolate', label: 'Chocolate' },
+  { value: 'strawberry', label: 'Strawberry' },
+  { value: 'vanilla', label: 'Vanilla' },
+];
 function App() {
+  const formik = useFormik({
+    initialValues: {
+      sth: 'test',
+      test: 'chocolate',
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
+
   return (
     <div className="page_wrapper">
+      <form onSubmit={formik.handleSubmit}>
+        <Select
+          options={options}
+          name="test"
+          onChange={(option) => formik.setFieldValue('test', option?.value)}
+        />
+        <input name='sth' onChange={formik.handleChange} value={formik.values.sth}/>
+        <button type="submit">submit</button>
+      </form>
     </div>
   );
 }
